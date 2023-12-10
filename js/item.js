ba6eb2b3d2a1e26aeb48d835f45e73f07be69252
lianjianhua <1813510519@qq.com>
2023-12-11 01:13:06 +0800

    将一些还在在html中的样式整合到css中，进行一些小优化

--- js/item.js
+++ js/item.js
@@ -4,9 +4,10 @@ console.log(id);
 // 读取 json 文件
 //将数据库中的数据赋予给网页对应位置
 $.getJSON("./data/item.json", function (data) {
-    var item = data[id-1];
-    var t=data[id-1].id
+    var item = data[id-1];  //itrm随机获得第一个id
+    var t=data[id-1].id     //将第一个id赋值给t
     console.log(item);
+    //每个元素赋予所对应的数据
     $("#item-name").text(item.name);
     $("#item-price").text(Number(item.price).toFixed(2));
     $("#item-description").text(item.description);
@@ -14,12 +15,14 @@ $.getJSON("./data/item.json", function (data) {
     $("#class").text(item.class);
     $("#condition").text(item.condition);
     $("#function").text(item.function);
-    const category=data[id-1].category;
+    const category=data[id-1].category;//获得刚刚随机到的id-1的对应数据库中的产品类型区间
     data=data.filter(function(item){
-        return item.id!=t;
+        return item.id!=t;//利用filter删除刚刚已获得的原始id
     });
+    //将所获的区间的数组打乱
     data.sort(function () {
         return (0.5 - Math.random());
     });
+    //将其他已打乱的id与所获类型的区间用函数调用呈现，使他不会总是千篇一律的呈现同样的排序
     printItems(data,category);
 }); 
