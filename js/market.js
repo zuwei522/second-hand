db999af2b68d9115dea4572940fedebe712e3b33
Yangjunjie <2771418664@qq.com>
2023-12-14 12:55:46 +0800

    注释更新

--- js/market.js
+++ js/market.js
@@ -7,17 +7,9 @@ $(document).ready(function () {
     $("#searchInput").on('input', function () {
         // 如果搜索框不为空，执行以下函数
         if (this.value.length > 0) {
-            // 将搜索框的值转换为小写，这里this指代json里的数据块即
-            //     {
-            //     "id": 例子,
-            //     "name": "例子",
-            //     "price": "2例子",
-            //     "description": "例子。",
-            //     "category": "例子",
-            //     "src":"例子",
-            //     "qq":"例子"
-            //   },
-            var searchText = $(this).val().toLowerCase();//toLowerCase()将字符串转化成小写
+            // 将this的值转换为小写，这里this指代输入框的输入数据。
+            //toLowerCase()将字符串转化成小写，searchText来保存搜索内容
+            var searchText = $(this).val().toLowerCase();
             // 初始化一个空数组，用于存储搜索结果
             var searchResults = [];
             // 发送一个Ajax请求，从./data/item.json获取数据
@@ -40,14 +32,17 @@ $(document).ready(function () {
                 // 遍历搜索结果数组，将每个结果添加到搜索结果列表中
                 //index相当于次数
                 $.each(searchResults, function (index) {
-                    console.log("现在遍历到了第 " + index + " 个搜索结果");//控制面板能看到
+                    //打印到控制面板,控制面板查询当前状态
+                    console.log("现在遍历到了第 " + index + " 个搜索结果");
                     // 将每个结果的名称和ID添加到搜索结果列表中
                     $("#search_results").append(
                         `<a class="searchResultItem list-group-item 
-                    list-group-item-light list-group-item-action text-decoration-none" href='./item.html?id=${this.id}'>${this.name}<a/>`);
+                    list-group-item-light list-group-item-action text-decoration-none" 
+                    href='./item.html?id=${this.id}'>${this.name}<a/>`);
                 });
 
                 // 绑定搜索结果列表中的每个链接，以便在左键单击时导航到相应的页面
+                //mousedow事件在定点设备（如鼠标或触摸板）按钮在元素内按下时，会在被绑定的元素上触发
                 $('.searchResultItem').on('mousedown', function () {
                     //从列表对应的a标签获取链接
                     const target = $(this).attr('href');
@@ -68,14 +63,12 @@ $(document).ready(function () {
     });
 
     // 当搜索框获得焦点时，显示搜索结果列表
-    //同理
+    //同理bootstrap里有一个d-none的一个类，利用removeClass给id为search_results删除d-none从而达到隐藏的效果
     $('#searchInput').on('focus', function () {
         $("#search_results").removeClass("d-none");
     });
 
 });
-
-
 
 // 页面加载完毕后执行以下函数
 $(document).ready(function () {
@@ -125,12 +118,12 @@ $(document).ready(function () {
                                 searchResults.push(data[name]);
                             }
                         }
-                        // 输出搜索结果数组
+                        // 在控制面板输出搜索结果数组(检查的时候使用)
                         console.log(searchResults);
                         if (searchResults.length > 0) {
                             // 遍历搜索结果数组，将每个结果添加到搜索结果列表中
                             $.each(searchResults, function (index, abc) {
-                                //控制面板打印的内容
+                                //在控制面板输出搜索结果数组(检查的时候使用)
                                 console.log("现在遍历到了第 " + index + " 个搜索结果");//浏览器检查搜索结果是否正确
                                 // 将每个结果的名称和ID添加到搜索结果列表中
                                 //jquery里的一个方法，在元素的末尾插入内容（html）
@@ -160,13 +153,19 @@ $(document).ready(function () {
                         else {
                             $(".product_display_area").append(
                                 `<div class="alert alert-info alert-dismissible fade show">
-                                <strong style="font-size: 25pt;"><svg xmlns="http://www.w3.org/2000/svg" fill="#CC3545" height="50" width="50" viewBox="0 0 512 512"><path d="M256 32c14.2 0 27.3 7.5 34.5 19.8l216 368c7.3 12.4 7.3 27.7 .2 40.1S486.3 480 472 480H40c-14.3 0-27.6-7.7-34.7-20.1s-7-27.8 .2-40.1l216-368C228.7 39.5 241.8 32 256 32zm0 128c-13.3 0-24 10.7-24 24V296c0 13.3 10.7 24 24 24s24-10.7 24-24V184c0-13.3-10.7-24-24-24zm32 224a32 32 0 1 0 -64 0 32 32 0 1 0 64 0z"/></svg>
+                                <strong style="font-size: 25pt;"><svg xmlns="http://www.w3.org/2000/svg" 
+                                fill="#CC3545" height="50" width="50" viewBox="0 0 512 512">
+                                <path d="M256 32c14.2 0 27.3 7.5 34.5 19.8l216 368c7.3 12.4 7.3 27.7 .2 40.1S486.3 480 472 
+                                480H40c-14.3 0-27.6-7.7-34.7-20.1s-7-27.8 .2-40.1l216-368C228.7 39.5 241.8 32 256 32zm0 
+                                128c-13.3 0-24 10.7-24 24V296c0 13.3 10.7 24 24 24s24-10.7 24-24V184c0-13.3-10.7-24-24-24zm32 
+                                224a32 32 0 1 0 -64 0 32 32 0 1 0 64 0z"/></svg>
                                     很抱歉没有找到与“${searchKey}”相关的物品！！！</strong>
                                 <button type="button" class="close" data-dismiss="alert"></button>
                             </div>`)
                         }
                     });
-                } else {
+                }
+                else {
 
                     // 如果类别为其他，则显示所有类商品
                     printItems(data, '数码');
